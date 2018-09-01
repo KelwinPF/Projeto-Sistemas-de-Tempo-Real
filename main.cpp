703657153944eef47142863565c6bedf8fa6d4bd
KelwinPF <42890832+KelwinPF@users.noreply.github.com>
2018-09-01 19:24:06 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 #include "BlackGPIO/BlackGPIO.h"
 
 using namespace BlackLib;
+using namespace std;
 
 int main (int arcgc,char * argv[]){
-  std::cout << "Example 1 - GPIO (in/out)" << endl;
+  cout << "Example 1 - GPIO (in/out)" << endl;
   BlackGPIO saida(GPIO_14,output);
   BlackGPIO entrada(GPIO_67,output); 
   while(true){
-  std:string val = entrada.getvalue();
+  string val = entrada.getvalue();
    if(val=="1"){
       saida.getvalue(high);
-      std::cout << "high" << endl;
+      cout << "high" << endl;
    }else{
       saida.getvalue(low);
-      std::cout << "low" << endl;
+      cout << "low" << endl;
    }
   }
  return 0;

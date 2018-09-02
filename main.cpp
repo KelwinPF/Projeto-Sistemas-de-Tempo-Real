bdbff302cf05fd96540f6ce69d5a500774ca1d3a
KelwinPF <42890832+KelwinPF@users.noreply.github.com>
2018-09-02 09:41:48 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <thread>
+#include <pthread.h>
 #include "BlackGPIO/BlackGPIO.h"
 
 using namespace BlackLib;
 using namespace std;
 
+int rPinLed = 46;
+int rPinBt = 67;
+
+BlackGPIO entrada((gpioName)rPinBt,input);
+BlackGPIO saida ((gpioName)rPinLed,output);
+
 int main (int arcgc,char * argv[]){
+  
+  thread red (readInputs, &rBt, 0);
+  red.join();
+  
   cout << "Example 1 - GPIO (in/out)" << endl;
-  BlackGPIO saida(GPIO_46,output);
-  BlackGPIO entrada(GPIO_67,input); 
+
   while(true){
   string val = entrada.getValue();
    if(val=="1"){

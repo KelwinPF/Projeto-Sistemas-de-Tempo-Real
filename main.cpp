b8cda385f07297db0caec6b8b8130e4ce2da6624
KelwinPF <42890832+KelwinPF@users.noreply.github.com>
2018-10-06 11:51:57 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -74,5 +74,19 @@ int main(){
             }
     }
     exit(0);
+ 
+void carga(int k){
+    float f = 0.999999;
+    for(int i=0; i<k; i++){
+        f = f*f*f*f*f;
+        f = 1.56;
+        for(int j=0; j<k; j++) f = sin(f)*sin(f)*f*f*f;
+    }
 }
  
+int setCPU(int i){
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(i, &mask);
+    return sched_setaffinity(0, sizeof(mask), &mask);
+}
